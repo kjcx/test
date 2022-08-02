ce9fcbad7854b6407b31487406bc7ed3d3fba3be
dongkai <dongk@meixing.com>
2022-08-02 18:12:46 +0800

    update

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
-import "fmt"
-func main(){
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
     fmt.Println("123")
+    http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+        writer.Write([]byte("ok"))
+    })
+    http.ListenAndServe(":80", nil)
+
 }
